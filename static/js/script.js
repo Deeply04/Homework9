11b5ef5570e00c5d6107de93465f85cdb7b08b7c
Deeply04 <bakhutashvili19@gmail.com>
2022-09-04 15:59:09 +0400

    Homework9

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+const object = {
+    name: 'davit',
+    lastName: 'bakhutashvili',
+    city: 'Tbilisi',
+    age:22,
+    isMerried: false,
+    isGraduated: true,
+    drivingLicense: true,
+    proggramingLanguages: ['html', 'css', 'javascript']
+}
+
+
+console.log(object)
